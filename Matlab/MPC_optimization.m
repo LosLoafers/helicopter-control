a6a4af5f3d853331f706b45364df7347b6ec3b35
Sverre Knutsen <sverre.hknutsen@gmail.com>
2018-03-20 17:13:09 +0100

    Lade till lite komentarer och fixade så att funktionen linearDescreteModelGen returnerar de deiskreta model matriserna.

--- Matlab/MPC_optimization.m
+++ Matlab/MPC_optimization.m
@@ -1,3 +1,6 @@
+% Denna fil är mest tänkt som ett test av CVXgen 
+
+%någon random punkt
 x10=0;
 x20=0;
 x30=pi/4;
@@ -6,7 +9,9 @@ x50=u10/k1;
 x60=u20/k2;
 u10=10;
 u20=10;
-[A,B]=linearization(x10,x20,x30,x40,x50,x60,u10,u20);
+%samplingstid
+h=0.1;
+[A,B]=linearDescreteModelGen(x10,x20,x30,x40,x50,x60,u10,u20,h);
 
 %setup opt problem
 params.A=A;
@@ -18,12 +23,12 @@ params.u_min=-10;
 params.x_0=[x10,x20,x30,x40,x50,x60]';
 
 % Create a new settings structure, with some example settings.
-settings.verbose = 0;  % disable output of solver progress.
-settings.max_iters = 10;  % reduce the maximum iteration count, from 25.
-settings.eps = 0.1;  % reduce the required objective tolerance, from 1e-6.
-settings.resid_tol = 1e-2;  % reduce the required residual tolerances, from 1e-4.
+% settings.verbose = 0;  % disable output of solver progress.
+% settings.max_iters = 10;  % reduce the maximum iteration count, from 25.
+% settings.eps = 0.1;  % reduce the required objective tolerance, from 1e-6.
+% settings.resid_tol = 1e-2;  % reduce the required residual tolerances, from 1e-4.
 
-% solv
+% solve
 [vars, status] = csolve(params, settings);
 u=vars.u{1};
 x=vars.x;
--- Matlab/linearization.m
+++ Matlab/linearization.m
@@ -23,9 +23,11 @@ JL=ml/3*(l1^3+l2^3)/(l1+l2)+m1*l1^2+m2*l2^2+mw*lw^2;
 JA=0;
 lc=(ml*(l1-l2)+m1*l1-m2*l2-mw*lw)/m;
 
-syms x1 x2 x3 x4 x5 x6 f1 f2 f3 f4 u1 u2  
-%x1=phi x2=d/dt phi x3=theta x4=d/dt theta x5=w1 x6=w2
+syms x1 x2 x3 x4 x5 x6 f1 f2 f3 f4 u1 u2  %sympolics of all variabels/sates and inputs
+%the folowing notation is used:
+%x1=phi x2=d/dt(phi) x3=theta x4=d/dt(theta) x5=w1 x6=w2
 
+%nonlinearmodel
 %d/dt x1=x2
 %d/dt x2=f1
 %d/dt x3=x4
@@ -33,12 +35,13 @@ syms x1 x2 x3 x4 x5 x6 f1 f2 f3 f4 u1 u2
 %d/dt x5=f3
 %d/dt x6=f4
 
+%define f1
 f1=(2*cos(x3)*sin(x3)*x2*x4*JL-2*h*(sin(x3)^2-cos(x3)^2)*x2*x4*m*lc...
     -2*h^2*sin(x3)*cos(x3)*x2*x4*m+D1*x5*abs(x5)*cos(x3)...
     +l2*cos(x3)*C2*x6*abs(x6))/(cos(x3)^2*JL-2*h*cos(x3)*sin(x3)*m*lc+...
     h^2*sin(x3)^2*m+JA);
 
-
+%diffrentiate f1
 f1prime_x1=diff(f1,x1);
 f1prime_x2=diff(f1,x2);
 f1prime_x3=diff(f1,x3);
@@ -46,10 +49,11 @@ f1prime_x4=diff(f1,x4);
 f1prime_x5=diff(f1,x5);
 f1prime_x6=diff(f1,x6);
 
+%define f2
 f2=(-cos(x3)*sin(x3)*x2^2*JL-h*(-sin(x3)^2+cos(x3)^2)*x2^2*m*lc...
     -g*cos(x3)*m*lc+h^2*sin(x3)*cos(x3)*x2^2*m+m*g*h*sin(x3)+l1*C1*x5*abs(x5)...
     +D2*x6*abs(x6))/(JL+h^2*m);
-
+%diffrentiate f2
 f2prime_x1=diff(f2,x1);
 f2prime_x2=diff(f2,x2);
 f2prime_x3=diff(f2,x3);
@@ -57,29 +61,22 @@ f2prime_x4=diff(f2,x4);
 f2prime_x5=diff(f2,x5);
 f2prime_x6=diff(f2,x6);
 
+%define f3
 f3=-1/T1*x5+1/(T1*k1)*u1;
 
+%diffrentiate f3
 f3prime_x5=diff(f3,x5);
 f3prime_u1=diff(f3,u1);
 
-
+%define f4
 f4=-1/T2*x6+1/(T2*k2)*u2;
 
-
+%diffrentiate f4
 f4prime_x6=diff(f4,x6);
 f4prime_u2=diff(f4,u2);
 
-%lin point
-% x10=0;
-% x20=0;
-% x30=pi/4;
-% x40=0;
-% x50=u10/k1;
-% x60=u20/k2;
-% u10=10;
-% u20=10;
 
-%A matrix
+%forming the continious linear A matrix asuming x is a stationary point
 A=[0 1 0 0 0 0;
     f1prime_x1 f1prime_x2 f1prime_x3 f1prime_x4 f1prime_x5 f1prime_x6;
     0 0 0 1 0 0 ;
@@ -87,13 +84,13 @@ A=[0 1 0 0 0 0;
     0 0 0 0 f3prime_x5 0;
     0 0 0 0 0 f4prime_x6];
 
+%forming a functin from the symbolic expression 
 Afunction=matlabFunction(A);
 
-A=Afunction(x20,x30,x40,x50,x60)
+%returning the achived A matrix linearized around x.
+A=Afunction(x20,x30,x40,x50,x60);
 
 
-%B matrix
+%B matrix (B was already linear so this was unnessesary.... :P )
 Bfunction=matlabFunction([0 0;0 0;0 0;0 0;f3prime_u1 0; 0 f4prime_u2]);
 B=Bfunction();
-C=[1 0 1 0 0 0];
-linSystem=ss(A,B,C,0)
