20d5c9003aba3d6275f95eed798ee8165113efe7
Sverre Knutsen <sverre.hknutsen@gmail.com>
2018-03-26 22:10:22 +0200

    raderade grejor

--- Matlab/SimulinkMPCtest/Sinmulink_MPC_nonlinear_gainschedule_kalman/MPC_test_setup_nonlinear.m
+++ Matlab/SimulinkMPCtest/Sinmulink_MPC_nonlinear_gainschedule_kalman/MPC_test_setup_nonlinear.m
@@ -10,8 +10,8 @@ global reference;
 global LinearizationPoint_x;
 global LinearizationPoint_u;
 Ts_control=0.05; % sample time
-Qglobal=diag([10 10 10 10 0 0]); %weight on states
-Rglobal=eye(2)*10;     %weight on control signal
+Qglobal=diag([10 30 10 10 0 0]); %weight on states
+Rglobal=diag([1 1])*100;     %weight on control signal
 umaxglobal=[1,1]'*10; %constraint on control
 
 %initial linearization point
@@ -21,7 +21,7 @@ phi0=0;
 k1=1e-2;
 k2=1.39e-2;
 LinearizationPoint_u=[LinearizationPoint_x(1)*k1,LinearizationPoint_x(2)*k2]'; %store lin. u
-reference=[0,1,0,0,0,0]'; %in origninal state space
+reference=[0,-pi/8,0,0,0,0]'; %in origninal state space
 
 %save the linearization in global vaariabels avalible to simulink function.
 
@@ -30,6 +30,9 @@ reference=[0,1,0,0,0,0]'; %in origninal state space
 %initial condition 
 x_0=[0,0,0,0,0,0]; %LinearizationPoint_x;
 
+%initial u signal for startup
+[~,~,step_x]=linearDiscreteModelGen(0,pi/4.3,Ts_control);
+u_step=[step_x(5)*k1;step_x(6)*k2];
 %kalman setup
 global Q;
 global R;
