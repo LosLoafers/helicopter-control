f7f64c0551e1c1636203845d5963a030dca379cd
RobinSauer <sauerrobin@hotmail.com>
2018-04-13 13:34:11 +0200

    Add files via upload
    Julia implementering av matlab

---
+++ Helicopter/heli.jl
@@ -0,0 +1,15 @@
+include("Kalman.jl")
+include("cvx.jl")
+
+#---------------global variables------------------------------------------------
+x_hat=[0,-pi/4,0,0,0,0,0,0] #estimated states
+u=[0 0]' #control signal
+y=[0 0]' #measurement signal
+P=eye(8) #covariance matrix
+#--------------------------------------------------------------------------------
+
+#dessa två funktionerna är de enda som behövs i main
+#exempel på användning ->
+    #(x_hat,p)=updateKalman(x_hat,u,y,p)
+    #(x_hat,p)=updateKalman([1 2 3 4 5 6 7 8]',[1 1]',[1 2]',100*eye(8))
+    #u=cvxsolve(x_hat,ref)
