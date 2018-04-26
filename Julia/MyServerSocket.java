6ac5dd8dbec2ad89075ded6e91ecac4c77313c5b
emmaestet <emmaestet@gmail.com>
2018-04-26 16:41:36 +0200

    GUI

--- Julia/MyServerSocket.java
+++ Julia/MyServerSocket.java
@@ -5,51 +5,72 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+import java.lang.*;
 
 public class MyServerSocket {
     private ServerSocket server;
     private static Opcom opcom;
+    private double phi;
+    private double theta;
+    private double phiref;
+    private double thetaref;
+    private String data;
+    private PlotData pdTheta;
+    private PlotData pdPhi;
+
     public MyServerSocket(int portNumber) throws Exception {
-          this.server = new ServerSocket(portNumber, 1, InetAddress.getLocalHost());
+        this.server = new ServerSocket(portNumber, 1, InetAddress.getLocalHost());
     }
     private void listen() throws Exception {
-        String data = null;
         Socket client = this.server.accept();
         String clientAddress = client.getInetAddress().getHostAddress();
         System.out.println("\r\nNew connection from " + clientAddress);
 
         BufferedReader in = new BufferedReader(
-                new InputStreamReader(client.getInputStream()));
-				PrintWriter out = new PrintWriter(client.getOutputStream());
-                double time = 0;
-        PlotData pdTheta;
-        PlotData pdPhi;
-        while ( (data = in.readLine()) != null ) {
-            time += 0.01;   //0.005;
-            String[] parts = data.split("/");
-            double phi = Double.parseDouble(parts[0]);
-            double theta = Double.parseDouble(parts[1]);
-            double phiref = Double.parseDouble(parts[2]);
-            double thetaref = Double.parseDouble(parts[3]);
-                   pdTheta = new PlotData();
-                   pdTheta.y = theta;
-                   pdTheta.ref = thetaref;
-                   pdTheta.x = time;
-                   opcom.putThetaDataPoint(pdTheta);
-                   pdPhi = new PlotData();
-                   pdPhi.y = phi;
-                   pdPhi.ref = phiref;
-                   pdPhi.x = time;
-                   opcom.putPhiDataPoint(pdPhi);
+        new InputStreamReader(client.getInputStream()));
+        PrintWriter out = new PrintWriter(client.getOutputStream());
+        long dtime = System.currentTimeMillis();
 
-
-
-						String type = data.getClass().getName();
-            System.out.println("\r\nMessage from " + clientAddress + ": " + data + "\nMessage type: " + type);
-						out.println("\r\nYou sent: " + data + "\nMessage type: " + type);
-						out.flush();
-				}
+        readIn(in);
+        long h = System.currentTimeMillis();
+        plot(0);
+        long time = 0;
+        while (true) {
+            readIn(in);
+            dtime = System.currentTimeMillis() - h;
+            h = System.currentTimeMillis();
+            time = time + dtime;
+            plot(time/1000.0);
+        }
     }
+
+
+    private void readIn(BufferedReader in) {
+        try {
+            this.data = in.readLine();
+        }catch(Exception e) {
+            System.out.println(e);
+        }
+        String[] parts = data.split("/");
+        this.phi = Double.parseDouble(parts[0]);
+        this.theta = Double.parseDouble(parts[1]);
+        this.phiref = Double.parseDouble(parts[2]);
+        this.thetaref = Double.parseDouble(parts[3]);
+    }
+
+    private void plot(double dtime) {
+        pdTheta = new PlotData();
+        pdTheta.y = theta;
+        pdTheta.ref = thetaref;
+        pdTheta.x = dtime;
+        pdPhi = new PlotData();
+        pdPhi.y = phi;
+        pdPhi.ref = phiref;
+        pdPhi.x = dtime;
+        opcom.putThetaDataPoint(pdTheta);
+        opcom.putPhiDataPoint(pdPhi);
+    }
+
     public InetAddress getSocketAddress() {
         return this.server.getInetAddress();
     }
@@ -60,13 +81,13 @@ public class MyServerSocket {
     public static void main(String[] args) throws Exception {
         MyServerSocket app = new MyServerSocket(2018);
         System.out.println("\r\nRunning Server: " +
-                "Host=" + app.getSocketAddress().getHostAddress() +
-                " Port=" + app.getPort());
+        "Host=" + app.getSocketAddress().getHostAddress() +
+        " Port=" + app.getPort());
 
         opcom = new Opcom();
         opcom.initializeGUI();
         opcom.start();
         app.listen();
-
+        
     }
 }
