cb194932b7c60c700f0d1b4a5d32805371d633d5
Sverre Knutsen <sverre.hknutsen@gmail.com>
2018-05-08 14:34:12 +0200

    add tuple

--- Helicopter/spawnTest_withChannel.jl
+++ Helicopter/spawnTest_withChannel.jl
@@ -2,25 +2,27 @@
 
 
 @everywhere we_should_run=true
-xchannel=RemoteChannel(()->Channel{Int}(2));
+xchannel=RemoteChannel(()->Channel{Tuple}(2));
 uchannel=RemoteChannel(()->Channel{Int}(2));
   Su=ReentrantLock()
   Sx=ReentrantLock()
 #  Su=Mutex()
 #  Sx=Mutex()
 kalman=@spawnat 1 begin
-  x=0
+  x=[0.0 0.0];
 
-  put!(xchannel,0)
+  put!(xchannel,(0,0))
   put!(uchannel,0)
   #Su=SpinLock()
   #Sx=SpinLock()
-  we_should_run=true
+  #we_should_run=true
 
   while we_should_run
-    x=x+1
+
+    x=x+[1 1]
 
     lock(Su)
+
       if isready(uchannel)
         u=take!(uchannel)
       end
@@ -30,7 +32,7 @@ kalman=@spawnat 1 begin
       while isready(xchannel) #empty channel. only last result should be stored
         take!(xchannel)
       end
-      put!(xchannel,x)
+      put!(xchannel,(x[1],x[2]))
     unlock(Sx)
 
     println("on kalman ",u)
@@ -42,21 +44,22 @@ end
 
 MPC=@spawnat 2 begin
   u=0
-
-  put!(xchannel,0)
+  x_hat=[0 0]
+  put!(xchannel,(0,0))
   put!(uchannel,0)
   #Su=ReentrantLock()
   #Sx=ReentrantLock()
-  we_should_run=true
+  #we_should_run=true
 
   while we_should_run
+  println("on MPC start")
     u=u+1
     lock(Sx)
       if isready(xchannel)
-        x_hat=take!(xchannel)
+        x_hat1,x_hat2=take!(xchannel)
       end
     unlock(Sx)
-
+    x_hat=[x_hat1 x_hat2]
     lock(Su)
       while isready(uchannel) #empty channel. only last result should be stored
         take!(uchannel)
